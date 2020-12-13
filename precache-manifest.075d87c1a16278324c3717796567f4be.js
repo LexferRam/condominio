c121a676b17242d3b97bd82e105270c44850524d
Lexfer Ramirez <lramirez@spiramide.local>
2020-12-13 19:04:29 -0400

    Updates

---
+++ precache-manifest.075d87c1a16278324c3717796567f4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c6d25ed3a51b81f3f5b245e5a6a968",
+    "url": "/LexferRam/condominio/index.html"
+  },
+  {
+    "revision": "c21050a9973903f73477",
+    "url": "/LexferRam/condominio/static/js/2.14dbbb80.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/LexferRam/condominio/static/js/2.14dbbb80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c341604a2174d91cda",
+    "url": "/LexferRam/condominio/static/js/main.b484dc5a.chunk.js"
+  },
+  {
+    "revision": "f65bce05c546b728ee0b",
+    "url": "/LexferRam/condominio/static/js/runtime-main.40e41a61.js"
+  }
+]);
